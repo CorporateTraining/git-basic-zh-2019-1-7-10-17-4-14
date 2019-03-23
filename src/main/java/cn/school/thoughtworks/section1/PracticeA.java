4bf8b8f8dbd14e57618d8e285aba2d8c2acbf06c
mahuanfy <mahuanfy@foxmail.com>
2019-03-23 23:21:26 +0800

    [HuanHuan]: Finish collect same elements test

--- src/main/java/cn/school/thoughtworks/section1/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeA.java
@@ -1,10 +1,18 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PracticeA {
     List<String> collectSameElements(List<String> collection1, List<String> collection2) {
-        //实现练习要求，并改写该行代码。
-        return null;
+        List<String> collection = new ArrayList<>();
+        for (String item : collection1) {
+            for (String value : collection2) {
+                if (item.equals(value)) {
+                    collection.add(item);
+                }
+            }
+        }
+        return collection;
     }
 }
