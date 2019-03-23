64bbb5123644753b8115a6380c307d12e6370cd0
mahuanfy <mahuanfy@foxmail.com>
2019-03-23 23:52:59 +0800

    [HuanHuan]: Finish section1-practiceC

--- src/main/java/cn/school/thoughtworks/section1/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeC.java
@@ -1,11 +1,20 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
     List<String> collectSameElements(List<String> collection1, Map<String,List<String>> collection2) {
-        //实现练习要求，并改写该行代码。
-        return null;
+        List<String> collections = new ArrayList<>();
+
+        for (String collection : collection1) {
+            if (collection2.get("value").contains(collection)) {
+                collections.add(collection);
+            }
+
+        }
+
+        return collections;
     }
 }
