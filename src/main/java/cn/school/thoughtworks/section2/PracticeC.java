5b1b5f3ca3948b9caeda41b98ea82abc286620d1
mahuanfy <mahuanfy@foxmail.com>
2019-03-24 14:12:54 +0800

    [HuanHuan]: Finish section2-practiceC

--- src/main/java/cn/school/thoughtworks/section2/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeC.java
@@ -1,12 +1,39 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PracticeC {
     Map<String, Integer> countSameElements(List<String> collection1) {
-        //实现练习要求，并改写该行代码。
+        Map<String, Integer> result = new HashMap<>();
+        for (String collection : collection1) {
+            if (collection.length() > 1) {
+                Integer elementNumber = getElementNumber(collection);
+                String element = String.valueOf(collection.charAt(0));
+                getElementsCount(result, element, elementNumber);
+            } else {
+                getElementsCount(result, collection, 1);
+            }
+        }
 
-        return null;
+        return result;
+    }
+
+    private void getElementsCount(Map<String, Integer> result, String collection, int number) {
+        if (result.containsKey(collection)) {
+            result.put(collection, result.get(collection) + number);
+        } else {
+            result.put(collection, number);
+        }
+    }
+
+    private Integer getElementNumber(String collection) {
+        Pattern pattern = Pattern.compile("[^0-9]");
+        Matcher matcher = pattern.matcher(collection);
+
+        return Integer.valueOf(matcher.replaceAll(""));
     }
 }
